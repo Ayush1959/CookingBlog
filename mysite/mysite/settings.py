d302e3a2ee992f85f0e7d76561acd30dec6aad60
Ayush <srihari.ayush@gmail.com>
2018-08-05 18:09:59 +0530

    BLOG update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,13 +25,14 @@ SECRET_KEY = '2318ktz44*3a6x-3u_y3m%@_x9b-p!19-w56!!u-hq11i_8ia@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ayush1959.pythonanywhere.com']
+ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'personal',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
